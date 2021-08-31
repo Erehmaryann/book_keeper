469db39c3443d245413568745b058a1ef01274b5
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-31 15:09:33 -0700

    selected the show modal button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //Select all the needed elements in the document
 // assign the elements to a variable
 const modal = document.querySelector("#modal");
+const modalShow = document.querySelector("#showModal");
