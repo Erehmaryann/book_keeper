36b60fe02a2f27df82b24f27e436223eeba5e47c
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-31 15:17:45 -0700

    selected the website name element

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const modal = document.querySelector("#modal");
 const modalShow = document.querySelector("#showModal");
 const modalClose = document.querySelector("#closeModal");
 const bookmarkForm = document.querySelector("#bookmarkForm");
+const websiteNameEl = document.querySelector("#websiteName");
